8bb2091974ae3a54b645bcf677a70087bc72e695
rafael <ferreira.rafaelc@gmail.com>
2017-12-26 00:19:08 -0300

    setup navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
+
+ 	root to: "home#index"
+
   resources :transactions
   resources :accounts
   resources :agencies
   devise_for :usuarios
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
